a3ee8f6e855e1ca00c944a8687213eab519c3af9
LikidPro <115822539+LikidPro@users.noreply.github.com>
2023-03-07 16:40:53 -0300

    Second commit

---
+++ src/main/java/com/sping3/otro/curso/OtroCursoApplication.java
@@ -0,0 +1,13 @@
+package com.sping3.otro.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtroCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtroCursoApplication.class, args);
+	}
+
+}
